3d0aa1f6f98a938be4a922bf958a7bc21759e233
Steve Birtles <sbirtles@farnborough.ac.uk>
2018-11-20 14:17:34 +0000

    All done (except database)

--- main.go
+++ main.go
@@ -1,36 +1,38 @@
 package main
 
 import (
-	"net/http"
-	"fmt"
-	"strings"
-	"os"
 	"encoding/json"
+	"fmt"
 	"github.com/satori/go.uuid"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
 type Message struct {
-	Id int `json:"id"`
-	Text string `json:"text"`
+	Id       int    `json:"id"`
+	Text     string `json:"text"`
 	PostDate string `json:"postDate"`
-	Author string `json:"author"`
+	Author   string `json:"author"`
 }
 
 type User struct {
-	Id int `json:"id"`
-	Username string `json:"username"`
-	Password string `json:"password"`
+	Id           int    `json:"id"`
+	Username     string `json:"username"`
+	Password     string `json:"password"`
 	SessionToken string `json:"sessionToken"`
 }
 
 var (
-	messages = []Message{{1, "Testing", "Today", "Steve"}}
-	users = []User{{1, "Steve", "beans", ""}}
+	messages = make([]Message, 0)
+	users    = make([]User, 0)
 )
 
 func clientHandler(w http.ResponseWriter, r *http.Request) {
 
-	filePath :=  "." + r.URL.Path
+	filePath := "." + r.URL.Path
 
 	if !strings.HasPrefix(filePath, "./client/") {
 		fmt.Println("ERROR: Invalid client path", filePath)
@@ -38,17 +40,39 @@ func clientHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if strings.HasSuffix(filePath, "/") { filePath = filePath + "index.html" }
+	if strings.HasSuffix(filePath, "/") {
+		filePath = filePath + "index.html"
+	}
 
 	info, err := os.Stat(filePath)
 	if os.IsNotExist(err) || info.IsDir() {
-		fmt.Println("ERROR: File not found", filePath)
+		if !strings.HasSuffix(filePath, ".map") {
+			fmt.Println("ERROR: File not found", filePath)
+		}
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
 
 	fmt.Println("Serving file", filePath)
 	http.ServeFile(w, r, filePath)
+
+}
+
+func validateSessionToken(r *http.Request) string {
+
+	var sessionToken string
+	cookie, err := r.Cookie("sessionToken")
+	if err == nil {
+		sessionToken = cookie.Value
+	}
+
+	for i := range users {
+		if users[i].SessionToken == sessionToken {
+			return users[i].Username
+		}
+	}
+
+	return ""
 
 }
 
@@ -71,7 +95,35 @@ func newMessageHandler(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
-	fmt.Println("/message/new")
+
+	username := validateSessionToken(r)
+	if username == "" {
+		w.WriteHeader(http.StatusForbidden)
+		return
+	}
+
+	messageText := r.FormValue("messageText")
+
+	id := 0
+	for _, m := range messages {
+		if m.Id > id {
+			id = m.Id
+		}
+	}
+	id++
+
+	date := time.Now().Format("02-03-2006 15:04:05")
+
+	fmt.Println("/message/new", id, messageText, username, date)
+
+	messages = append(messages, Message{
+		Id:       id,
+		Text:     messageText,
+		PostDate: date,
+		Author:   username,
+	})
+	fmt.Fprint(w, "OK")
+
 }
 
 func deleteMessageHandler(w http.ResponseWriter, r *http.Request) {
@@ -80,7 +132,33 @@ func deleteMessageHandler(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
-	fmt.Println("/message/delete")
+
+	username := validateSessionToken(r)
+	id, err := strconv.Atoi(r.FormValue("messageId"))
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Println("/message/delete", username, id)
+
+	for i := 0; i < len(messages); {
+		if messages[i].Id == id {
+			if messages[i].Author != username {
+				fmt.Println(username, messages[i].Author)
+				fmt.Fprint(w, "Error: You didn't post that originally")
+				return
+			}
+			messages = append(messages[:i], messages[i+1:]...)
+			fmt.Fprint(w, "OK")
+			return
+		} else {
+			i++
+		}
+	}
+
+	fmt.Fprint(w, "Error: Can't find post with id ", id)
+
 }
 
 func editMessageHandler(w http.ResponseWriter, r *http.Request) {
@@ -89,7 +167,28 @@ func editMessageHandler(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
-	fmt.Println("/message/edit")
+
+	username := validateSessionToken(r)
+	id, err := strconv.Atoi(r.FormValue("messageId"))
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	newText := r.FormValue("messageText")
+
+	for i := range messages {
+		if messages[i].Id == id {
+			if messages[i].Author != username {
+				fmt.Fprint(w, "Error: You didn't post that originally")
+				return
+			}
+			messages[i].Text = newText
+		}
+	}
+
+	fmt.Println("/message/edit", username, id, newText)
+	fmt.Fprint(w, "OK")
 }
 
 func newUserHandler(w http.ResponseWriter, r *http.Request) {
@@ -98,7 +197,30 @@ func newUserHandler(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
-	fmt.Println("/user/new")
+
+	username := r.FormValue("username")
+	password1 := r.FormValue("password1")
+	password2 := r.FormValue("password2")
+
+	if password1 != password2 {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, "Error: Passwords don't match")
+		return
+	}
+
+	id := 0
+	for _, u := range users {
+		if u.Id > id {
+			id = u.Id
+		}
+	}
+	id++
+
+	token := uuid.Must(uuid.NewV4()).String()
+	users = append(users, User{id, username, password1, token})
+
+	fmt.Println("/user/new", username, password1)
+	fmt.Fprint(w, token)
 }
 
 func loginUserHandler(w http.ResponseWriter, r *http.Request) {
@@ -107,24 +229,24 @@ func loginUserHandler(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
-	fmt.Println("/user/login")
 
 	username := r.FormValue("username")
 	password := r.FormValue("password")
+	fmt.Println("/user/login", username, password)
 
 	for i := range users {
 		if users[i].Username == username {
-			if  users[i].Password != password {
-				fmt.Fprintln(w, "Error: Incorrect password")
+			if users[i].Password != password {
+				fmt.Fprint(w, "Error: Incorrect password")
 				return
 			}
 			token := uuid.Must(uuid.NewV4()).String()
 			users[i].SessionToken = token
-			fmt.Fprintln(w, token)
+			fmt.Fprint(w, token)
 			return
 		}
 	}
-	fmt.Fprintln(w, "Error: Can't find user account.")
+	fmt.Fprint(w, "Error: Can't find user account.")
 
 }
 
@@ -134,22 +256,11 @@ func getUserHandler(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
-	fmt.Println("/user/get")
 
-	var sessionToken string
-	cookie, err := r.Cookie("sessionToken")
-	if err != nil {
-		sessionToken = cookie.Value
-	}
+	username := validateSessionToken(r)
+	fmt.Println("/user/get", username)
+	fmt.Fprint(w, username)
 
-	for i := range users {
-		if users[i].SessionToken == sessionToken {
-			fmt.Fprintln(w, users[i].Username)
-			return
-		}
-	}
-
-	fmt.Fprintln(w, "")
 }
 
 func main() {
@@ -166,4 +277,5 @@ func main() {
 	http.HandleFunc("/user/get", getUserHandler)
 
 	http.ListenAndServe(":8081", http.DefaultServeMux)
+
 }
